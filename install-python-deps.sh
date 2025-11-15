f270c6f1d2fe7e649b57f1e77aba2f7a469307a7
hlcxpl <hlcxpl@gmail.com>
2025-11-15 03:20:44 -0300

    chore: Clean up repository by removing unnecessary files
    - Remove all markdown files except README.md
    - Remove utility scripts (.sh files)
    - Remove temporary files (nohup.out, create_sample_data.py)
    - Update .gitignore to prevent tracking these files in the future

--- install-python-deps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Script para instalar dependencias de Python en Ubuntu/Debian
-echo "Instalando dependencias para compilar Python..."
-apt update
-apt install -y make build-essential libssl-dev zlib1g-dev \
-libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
-libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev \
-libffi-dev liblzma-dev
-echo "Dependencias instaladas correctamente."
